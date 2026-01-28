b422887085ce14877b68be755b8518117e22558b
Lakshya <181981038+vylakshya@users.noreply.github.com>
2026-01-28 16:24:38 +0530

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,8 +1,12 @@
 #include<stdio.h>
+#include<unistd.h>
 #include<sys/mman.h>
+#include<fnctl.h>
+#include<linux/fb.h>
 
-int main(){
 
+int main(int argc, char *argv[]){
+    
     
 
     return 0;
