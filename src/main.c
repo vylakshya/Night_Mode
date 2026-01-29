62d6b70cb663ba29224036d4f8ad4ad89afd43e3
Lakshya <181981038+vylakshya@users.noreply.github.com>
2026-01-29 17:09:05 +0530

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,12 +2,19 @@
 #include<unistd.h>
 #include<sys/mman.h>
 #include<fnctl.h>
+#include<sys/stat.h>
 #include<linux/fb.h>
 
 
 int main(int argc, char *argv[]){
     
+    int framebuffer;
+    mode_t permissions;
+    int pixelsRead;
+    int flags;
+    permissions = S_IRUSR | S_IWUSR | S_IRGRP | S_WGRP | S_IWOTH | S_IROTH;
     
+    framebuffer = open("dev/fb0", S_WRONLY, permissions); 
 
     return 0;
 }
